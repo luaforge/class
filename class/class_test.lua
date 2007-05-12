340ee2b963a5459dbeb8dc9606c1040039496d58
Mark Gurevich <murk@luaforge.net>
2007-05-12 09:35:55 +0000

    *** empty log message ***

---
+++ class/class_test.lua
@@ -0,0 +1,97 @@
+
+require('luaunit')
+----
+require('class')
+----
+
+
+TestClass = {}
+
+function TestClass:test_class_created()
+  class "A"
+  assert(A, "NOT A")
+  -- check class name ?
+  A = nil
+end
+
+function TestClass:test_instance_created()
+  class "A"
+  local a = A()
+  assert(a, "NOT a")
+  -- check class ?
+  -- check class name ?
+  A = nil
+end
+
+function TestClass:test_method_called()
+  class "A" do
+    function A:f(x)
+      return self, x
+    end
+  end
+  local x = {}
+  local a = A()
+  local _self, _x = a:f(x)
+  assert(a==_self, "a ~= _self")
+  assert(x==_x, "x ~= _x")
+  A = nil
+end
+
+function TestClass:test_initializer_called()
+  class "A" do
+    function A:initialize(x)
+      self.x = x
+    end
+  end
+  local x = {}
+  local a = A(x)
+  assert(a.x==x, "a.x ~= x")
+  A = nil
+end
+
+function TestClass:test_class_derived()
+  class "A" do
+    function A:f(x)
+      return x
+    end
+  end
+  class "B" (A)
+  local x = {}
+  local b = B()
+  local _x = b:f(x)
+  assert(x==_x, "x ~= _x")
+  A = nil
+  B = nil
+end
+
+function TestClass:test_metamethods()  --FIXME: write test for each meta-method
+  local x = {}
+  class "A" do
+    function A:__add__(other)
+      return x
+    end
+  end
+  assert(A()+{}==x, "A()+{} ~= x")
+  assert({}+A()==x, "{}+A() ~= x")
+  A = nil
+end
+
+function TestClass:test_inherits()
+  class "A"
+  class "B" (A)
+  class "C"
+  local b = B()
+  assert(b:inherits(B), "NOT b:inherits(B)")
+  assert(b:inherits(A), "NOT b:inherits(A)")
+  assert(not b:inherits(C), "YES b:inherits(C)")
+  --assert(B:inherits(A), "NOT B:inherits(A)")  --it is actually `implements()'
+  A, B, C = nil
+end
+
+--function TestClass:test_index()
+--function TestClass:test_newindex()
+
+--function TestClass:test_?()
+
+
+LuaUnit:run()
