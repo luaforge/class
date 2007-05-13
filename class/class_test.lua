0361b37be07f83da6314ced0939204814e129183
Mark Gurevich <murk@luaforge.net>
2007-05-13 19:27:55 +0000

    added tests for each supported meta-method

--- class/class_test.lua
+++ class/class_test.lua
@@ -1,8 +1,6 @@
 
 require('luaunit')
-----
 require('class')
-----
 
 
 TestClass = {}
@@ -10,7 +8,6 @@ TestClass = {}
 function TestClass:test_class_created()
   class "A"
   assert(A, "NOT A")
-  -- check class name ?
   A = nil
 end
 
@@ -18,8 +15,6 @@ function TestClass:test_instance_created()
   class "A"
   local a = A()
   assert(a, "NOT a")
-  -- check class ?
-  -- check class name ?
   A = nil
 end
 
@@ -64,17 +59,124 @@ function TestClass:test_class_derived()
   B = nil
 end
 
-function TestClass:test_metamethods()  --FIXME: write test for each meta-method
-  local x = {}
-  class "A" do
-    function A:__add__(other)
-      return x
+---- META-METHODS ----
+
+local binoperators = {}
+function binoperators.add(x,y) return x + y end
+function binoperators.sub(x,y) return x - y end
+function binoperators.mul(x,y) return x * y end
+function binoperators.div(x,y) return x / y end
+function binoperators.pow(x,y) return x ^ y end
+function binoperators.concat(x,y) return x..y end
+
+function TestClass:test_binoperators()
+  local t = {}
+  class "A"
+  for name in pairs(binoperators) do
+    A['__'..name..'__'] = function(x,y)
+      return t
     end
   end
-  assert(A()+{}==x, "A()+{} ~= x")
-  assert({}+A()==x, "{}+A() ~= x")
+  class "B"
+  for _, f in pairs(binoperators) do
+    local _t
+    _t = f(A(),A())
+    assertEquals(_t,t)
+    _t = f(A(),B())
+    assertEquals(_t,t)
+    _t = f(B(),A())
+    assertEquals(_t,t)
+    _t = f(A(),{})
+    assertEquals(_t,t)
+    _t = f({},A())
+    assertEquals(_t,t)
+    _t = f(A(),nil)
+    assertEquals(_t,t)
+    _t = f(nil,A())
+    assertEquals(_t,t)
+    assertError(f,B(),B())
+    assertError(f,B(),{})
+    assertError(f,{},B())
+  end
+  A, B = nil
+end
+
+local compoperators = {}
+function compoperators.lt(x,y) return x < y end
+function compoperators.le(x,y) return x <= y end
+function compoperators.eq(x,y) return x == y end
+
+function TestClass:test_compoperators()
+  class "A"
+  for name in pairs(compoperators) do
+    A['__'..name..'__'] = function(x,y)
+      return true
+    end
+  end
+  class "B"
+  for _, f in pairs(compoperators) do
+    assert(f(A(),A()))
+    assert(f(A(),B()))
+    --assert(f(B(),A()))      -- 'cause Object:__eq__
+    --assertError(f,B(),B())
+  end
+  A, B = nil
+end
+
+function TestClass:test_tostring()
+  class "A"
+  function A:__tostring__()
+    return "ABC"
+  end
+  assertEquals(tostring(A()),"ABC")
   A = nil
 end
+
+function TestClass:test_unm()
+  local t = {}
+  class "A"
+  function A:__unm__()
+    return t
+  end
+  assertEquals(-A(),t)
+  A = nil
+end
+
+function TestClass:test_newindex()
+  class "A"
+  function A:__newindex__(name,value)
+    rawset(self,name,value)
+  end
+  local a = A()
+  a.z = 13
+  assertEquals(a.z,13)
+  A = nil
+end
+
+function TestClass:test_index()
+  class "A"
+  function A:__index__(name)
+    return name
+  end
+  assertEquals(A().x,'x')
+  assertEquals(A()[10],10)
+  assertEquals(A()[A],A)
+  A = nil
+end
+
+function TestClass:test_call()
+  class "A"
+  function A:__call__(x,y)
+    return x, y
+  end
+  local x, y = {}, {}
+  local _x, _y = A()(x,y)
+  assertEquals(_x,x)
+  assertEquals(_y,y)
+  A = nil
+end
+
+--------------
 
 function TestClass:test_inherits()
   class "A"
@@ -104,12 +206,29 @@ function TestClass:test_super()
   A, B = nil
 end
 
---function TestClass:test_index()
---function TestClass:test_newindex()
+--[[       --currently fails--
+function TestClass:test_class_non_method_accessible()
+  local x = {}
+  class "A"
+  A.x = x
+  assertEquals(A.x,nil)
+  assertEquals(A().x,x)
+  A = nil
+end
+--]]
+
+
+
+-- TODO: test classes API
+
 --function TestClass:test_Class_derives()
 --function TestClass:test_Class_findmethod()
 
---function TestClass:test_?()
+--[[
+function TestClass:test_?()
+  ....
+end
+--]]
 
 
 LuaUnit:run()
